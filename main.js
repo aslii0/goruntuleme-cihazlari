a75e387d20b6f432961eb0007f081eec36ee0209
Aslı <109468411+aslii0@users.noreply.github.com>
2022-08-30 18:17:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const menuBTn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBTn.addEventListener('click' , () => {
+    if(!menuOpen) {
+        menuBTn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBTn.classList.remove('open');
+        menuOpen = false;
+    }
+});
